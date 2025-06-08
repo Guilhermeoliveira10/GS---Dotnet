b285c47b608dc688374fe597285bba151fd5e9d7
Guilherme <guizin05.oliveira@gmail.com>
2025-06-08 18:06:06 -0300

    Corrige formato da data nos modelos Alert e HelpRequest

--- GS - Dotnet/SafeZone.Domain/Entities/Alert.cs
+++ GS - Dotnet/SafeZone.Domain/Entities/Alert.cs
@@ -1,11 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SafeZone.Domain.Entities;
 
 public class Alert
 {
     public int Id { get; set; }
+
     public string Titulo { get; set; } = string.Empty;
+
     public string Tipo { get; set; } = string.Empty;
+
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     public DateTime Data { get; set; } = DateTime.Now;
 }
--- GS - Dotnet/SafeZone.Domain/Entities/HelpRequest.cs
+++ GS - Dotnet/SafeZone.Domain/Entities/HelpRequest.cs
@@ -1,8 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SafeZone.Domain.Entities;
+
 public class HelpRequest
 {
     public int Id { get; set; }
+
     public string Nome { get; set; } = string.Empty;
+
     public string TipoAjuda { get; set; } = string.Empty;
+
     public string Localizacao { get; set; } = string.Empty;
+
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     public DateTime DataSolicitacao { get; set; } = DateTime.Now;
 }
